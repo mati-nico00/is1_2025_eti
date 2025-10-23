8c8a4568895b8076d18f7e6a9ce94b96c990cb20
pponzio <pponzio@dc.exa.unrc.edu.ar>
2025-10-23 08:06:39 -0300

    Initial Commit

--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -7,3 +7,16 @@ CREATE TABLE users (
     name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
     password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
 );
+
+-- Elimina la tabla 'teachers' si ya existe
+DROP TABLE IF EXISTS teachers;
+
+-- Crea la tabla 'teachers' con los campos originales, adaptados a SQLite
+CREATE TABLE teachers(
+    id INTEGER,
+    apellido TEXT,
+    correo TEXT NOT NULL UNIQUE,
+    dni BIGINT UNIQUE,
+
+    CONSTRAINT fk_teachers_users PRIMARY KEY (id) REFERENCES users (id)
+);
